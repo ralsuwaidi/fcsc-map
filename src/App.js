cdf7fd7bf03aebe1d9081e7ff0c49292319749d7
Rashed AlSuwaidi <rashed.alsuwaidi@ai.gov.ae>
2023-09-14 09:53:31 +0400

    add icon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import Map from "./Map";
-import Search from "./components/Search";
 import Sidebar from "./components/Sidebar";
-import ButtonWithIcon from "./components/ButtonIcon";
+import { Button } from 'flowbite-react';
+import { HiShoppingCart } from 'react-icons/hi';
+import { BiWorld } from 'react-icons/bi';
+import { BsFillDoorOpenFill } from 'react-icons/bs';
+import { FaMoneyBillAlt } from 'react-icons/fa';
 
 const buttonList = [
   'Culture',
@@ -14,7 +17,6 @@ const buttonList = [
 
 function App() {
   const [isSidebarVisible, SidebarVisible] = useState(false);
-
   const handleButtonClick = () => {
     SidebarVisible(!isSidebarVisible);
   }
@@ -23,23 +25,27 @@ function App() {
     <div className="h-full w-full">
       <Map />
 
-      <div className="absolute left-4 top-4">
 
-        <div className="mt-4">
-          <ButtonWithIcon onClick={handleButtonClick} /> {/* Trigger sidebar visibility toggle */}
-          {isSidebarVisible && <Sidebar />} {/* Only render Sidebar when isSidebarVisible is true */}
-        </div>
+
+      <div className="absolute left-4 top-4 bg-white rounded">
+        <Button className='bg-white' size={'sm'} onClick={handleButtonClick}>
+          <BiWorld className=" text-black h-4 w-4" />
+        </Button>
+        <Button className='bg-white' size={'sm'} onClick={handleButtonClick}>
+          <BsFillDoorOpenFill className=" text-black h-4 w-4" />
+        </Button>
+        <Button className='bg-white' size={'sm'} onClick={handleButtonClick}>
+          <FaMoneyBillAlt className=" text-black h-4 w-4" />
+        </Button>
+        {isSidebarVisible && <Sidebar />} {/* Only render Sidebar when isSidebarVisible is true */}
       </div>
-
 
       <div className="absolute left-4 bottom-4">
         <div className="relative">
-          {/* <Search /> */}
-
 
           {buttonList.map((button) => (
             <div key={button} >
-              <p className="p-1 bg-white shadow-lg font-semibold mt-2 rounded-md w-fit">{button}</p>
+              <p className="p-1 bg-white shadow-lg font-semibold mt-2 rounded w-fit">{button}</p>
             </div>
           ))}
 
