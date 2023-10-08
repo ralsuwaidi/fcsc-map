0c6caeee88c4653b5fd9be9f22c8e53e9142ef60
Rashed AlSuwaidi <rashed.alsuwaidi@ai.gov.ae>
2023-10-08 10:47:44 +0400

    add 3D

--- src/Map.js
+++ src/Map.js
@@ -34,7 +34,7 @@ mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
 const Map = ({ filter, setDrawerOpen, setTabNumber }) => {
   const mapContainerRef = useRef(null);
   const [isOpen, setIsOpen] = useState(false);
-  const [modalContent, setModalContent] = useState(null);
+  const [modalContent, _] = useState(null);
 
   const createFilter = (filter) => {
     if (!filter) return null;
@@ -132,6 +132,56 @@ const Map = ({ filter, setDrawerOpen, setTabNumber }) => {
       // Clean up on unmount
       return () => map.remove();
 
+    });
+
+
+    map.on('style.load', () => {
+      // Insert the layer beneath any symbol layer.
+      const layers = map.getStyle().layers;
+      const labelLayerId = layers.find(
+        (layer) => layer.type === 'symbol' && layer.layout['text-field']
+      ).id;
+
+      // The 'building' layer in the Mapbox Streets
+      // vector tileset contains building height data
+      // from OpenStreetMap.
+      map.addLayer(
+        {
+          'id': 'add-3d-buildings',
+          'source': 'composite',
+          'source-layer': 'building',
+          'filter': ['==', 'extrude', 'true'],
+          'type': 'fill-extrusion',
+          'minzoom': 15,
+          'paint': {
+            'fill-extrusion-color': '#aaa',
+
+            // Use an 'interpolate' expression to
+            // add a smooth transition effect to
+            // the buildings as the user zooms in.
+            'fill-extrusion-height': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15,
+              0,
+              15.05,
+              ['get', 'height']
+            ],
+            'fill-extrusion-base': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15,
+              0,
+              15.05,
+              ['get', 'min_height']
+            ],
+            'fill-extrusion-opacity': 0.6
+          }
+        },
+        labelLayerId
+      );
     });
 
 
